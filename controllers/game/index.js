6fbcacbc5fb1f840f5d789fb4a3ac9dbe9b439a4
Fernando Reis Guimarães <fernandobhz@gmail.com>
2019-08-07 16:03:59 -0300

    wip: error cheking

--- controllers/game/index.js
+++ controllers/game/index.js
@@ -11,16 +11,16 @@ function gameFactory() {
 		x0y2: null,
 		x1y2: null,
 		x2y2: null,
-		
+
 		x0y1: null,
 		x1y1: null,
 		x2y1: null,
-				
+
 		x0y0: null,
 		x1y0: null,
 		x2y0: null,
 	}
-	
+
 	return game;
 }
 
@@ -34,13 +34,53 @@ exports.listGames = function(req, res, next) {
   res.json(games);
 }
 
-exports.movement = function(req, res, next) {	
-	//var game = games.find(x => x.id = req.params.id);
-	//res.json(game);
-	//return;
+exports.movement = function(req, res, next) {
+	var { id, player, position } = req.body;
+	var { x, y } = position;
+	
+	if ( id != req.params.id ) {
+		res.status(500).send('Url ID does not match the body ID');
+		return;
+	}
 
+	var game = games.find(x => x.id == req.params.id);
+
+	if ( ! game ) {
+		res.status(500).send('Could not found the game by the id provided');
+		return;
+	}
 	
+	if ( player != 'X' or player != 'O' ) {
+		res.status(500).send('Player must be O or X');
+		return;
+	}
 	
+	if ( x > 2 ) {
+		res.status(500).send('x value must be equal or less then 2');
+		return;		
+	}
+	
+	if ( y > 2 ) {
+		res.status(500).send('y value must be equal or less then 2');
+		return;		
+	}
+	
+	if ( x < 0 ) {
+		res.status(500).send('x value must greater then 0');
+		return;		
+	}
+	
+	if ( y < 0 ) {
+		res.status(500).send('y value must greater then 0');
+		return;		
+	}
+	
+	game.table['x' + x + 'y' + y] = player;
+	
+
+	res.json(game);
+	return;
+
 	req.body.method = 'movement';
 	req.body.id = req.params.id;
 	res.json(req.body);
