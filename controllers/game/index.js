8b71230e8c03e9b2a9eb8365a26f7bf99f693f38
Fernando Reis Guimarães <fernandobhz@gmail.com>
2019-08-07 15:40:19 -0300

    wip: movement method

--- controllers/game/index.js
+++ controllers/game/index.js
@@ -1,27 +1,46 @@
-const uuidv1 = require('uuid/v1');
+const uuidv4 = require('uuid/v4');
 
-var games = [{id: 1, firstPlayer: 'X'}];
-var winPossibilities = [];
+var games = [];
+var winPossibilities = [{x:0, y:1}];
 
 function gameFactory() {
-	var game = {};	
-	game.id = uuidv1();	
+	var game = {};
+	game.id = uuidv4();
 	game.firstPlayer = (Math.random() < 0.5 ? 'X' : 'O');
+	game.table = {
+		x0y2: null,
+		x1y2: null,
+		x2y2: null,
+		
+		x0y1: null,
+		x1y1: null,
+		x2y1: null,
+				
+		x0y0: null,
+		x1y0: null,
+		x2y0: null,
+	}
 	
 	return game;
 }
 
 exports.newGame = function(req, res, next) {
 	var game = gameFactory();
-	games.push(game);	
+	games.push(game);
   res.json(game);
 }
 
-exports.listGames = function(req, res, next) {	
+exports.listGames = function(req, res, next) {
   res.json(games);
 }
 
-exports.movement = function(req, res, next) {
+exports.movement = function(req, res, next) {	
+	//var game = games.find(x => x.id = req.params.id);
+	//res.json(game);
+	//return;
+
+	
+	
 	req.body.method = 'movement';
 	req.body.id = req.params.id;
 	res.json(req.body);
