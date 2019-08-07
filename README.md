965187c7404db707992fcf9f5be6b349cc625efd
Fernando Reis Guimarães <fernandobhz@gmail.com>
2019-08-07 16:39:33 -0300

    wip: missing the winner and draw methods

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # tictactoe
 
+# Requisitos
+* node v8
+
+# Instalação das dependências
+npm install
+
+# Execução
+npm start
+
+# Como usar
+Apesar de não solicitado, desenvolvi um front-end bem simples
+para testar a api, basta acessar o browser em http://localhost:3000
+
--- controllers/game/index.js
+++ controllers/game/index.js
@@ -9,6 +9,7 @@ function gameFactory() {
 	game.firstPlayer = (Math.random() < 0.5 ? 'X' : 'O');
 	game.previousPlayer = null;
 	game.nextPlayer = game.firstPlayer;
+	game.valid = true;
 	game.table = {
 		x0y2: null,
 		x1y2: null,
@@ -37,9 +38,13 @@ exports.listGames = function(req, res, next) {
 }
 
 exports.movement = function(req, res, next) {
+	// Received parameters
 	var { id, player, position } = req.body;
 	var { x, y } = position;
 	
+	
+	
+	// Validations
 	if ( id != req.params.id ) {
 		res.status(500).json({msg: 'O ID do jogo na url não corresponde ao ID passado na corpo da requisição'});
 		return;
@@ -52,37 +57,75 @@ exports.movement = function(req, res, next) {
 		return;
 	}
 	
+	if ( game.valid == false ) {
+		res.status(500).json({msg: 'Este jogo não é valido'});
+		return;
+	}
+
 	if ( player != 'X' && player != 'O' ) {
 		res.status(500).json({msg: 'Jogador deve ser X ou O, recebido: ' + player});
 		return;
 	}
-	
+
 	if ( player != game.nextPlayer ) {
 		res.status(500).json({msg: 'Não é turno do jogador'});
 		return;
 	}
-	
+
 	if ( x > 2 ) {
 		res.status(500).json({msg: 'O valor de x deve ser menor ou igual a 2, recebido: ' + x});
-		return;		
+		return;
 	}
-	
+
 	if ( y > 2 ) {
 		res.status(500).json({msg: 'O valor de y deve ser menor ou igual a 2, recebido:' + y});
-		return;		
+		return;
 	}
-	
+
 	if ( x < 0 ) {
 		res.status(500).json({msg: 'O valor de x deve ser maior que 0, recebido: ' + x});
-		return;		
+		return;
 	}
-	
+
 	if ( y < 0 ) {
-	res.status(500).json({msg: 'O valor de y deve ser maior que 0, recebido: ' + y});
-		return;		
+		res.status(500).json({msg: 'O valor de y deve ser maior que 0, recebido: ' + y});
+		return;
 	}
 	
+	if ( game.table['x' + x + 'y' + y] != null) {
+		res.status(500).json({msg: 'Esta jogada já foi feita, contate o suporte do front-end'});
+		game.valid = false;
+		return;
+	}
+	
+	
+	
+	// Exchange the current player
+	if ( player == 'X' ) {
+		game.previousPlayer = player;	
+		game.nextPlayer = 'O';
+	}	else if ( player == 'O' ) {
+		game.previousPlayer = player;	
+		game.nextPlayer = 'X'
+	}	else {
+		// extra validation, theoretically not needed
+		res.status(500).json({msg: 'Erro interno, contate o suporte'});
+		return;
+	}
+	
+	
+	
+	// Doing the movement
 	game.table['x' + x + 'y' + y] = player;
+	
+	
+	
+	// Cheking if current player has won
+	// n < 5 não é necessário
+	
+	
+	
+	// Check if game finished by draw
 	
 	res.json(game);
 	return;
